6232df6fc9a16df8f7ab0cc3989241961ac3f5fc
NicholasZeitoun <nzeit8003@gmail.com>
2020-11-01 23:27:25 +1100

    Added markdown display to notes

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,8 @@
 class Note < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+
+  def markdown_content
+    Kramdown::Document.new(content).to_html.html_safe
+  end
 end
