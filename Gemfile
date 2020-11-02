52942d304389fc2d351475a9686625204c7dfd41
NicholasZeitoun <nzeit8003@gmail.com>
2020-11-02 19:41:45 +1100

    Replaced markdown with trix

--- Gemfile
+++ Gemfile
@@ -63,3 +63,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Gem for markdown content
 gem 'kramdown'
+gem 'ace-rails-ap'
+gem 'bootstrap_form'
+gem 'bootstrap-sass', '~> 3.3.0'
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,7 @@
 class Note < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
+  has_rich_text :content
 
   def markdown_content
     Kramdown::Document.new(content).to_html.html_safe
