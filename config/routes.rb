e92946ad0565525e13d2a51d4b6c65f3a3e6565c
NicholasZeitoun <nzeit8003@gmail.com>
2020-11-02 23:00:34 +1100

    Added concepts model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :concepts
   resources :notes
   # root to: 'pages#home'
-  root to: 'notes#index'
+  root to: 'concepts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
